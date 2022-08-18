b2cdc57062fe6c72798877398eead4e19ad7ff91
Renan Cardoso <renan.cardoso@bycoders.com.br>
2022-08-18 15:41:35 -0300

    auth via TokenAuthentication

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 
+from .authentication import CustomAuthToken
 from .views import HelloViewSet
 
 from django.urls import path
@@ -6,5 +7,6 @@ from django.urls import path
 hello_world = HelloViewSet.as_view({'get': 'list'})
 
 urlpatterns = [
-    path(r'', hello_world)
+    path(r'', hello_world),
+    path('auth/', CustomAuthToken.as_view())
 ]
--- dashboard_api/settings.py
+++ dashboard_api/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'core'
+    'core',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
