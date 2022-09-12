f6f6f4a87bbd742fe955295b87e545de00726414
Renan Cardoso <renan.cardoso@bycoders.com.br>
2022-09-12 11:03:58 -0300

    payment history chart view

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 
-from core.views import PaymentSummaryCards, PaymentRecentsTable
-
+from core.views import PaymentSummaryCards, PaymentRecentsTable, PaymentTrackerChart
 from django.urls import path
 
 payments_summary_cards = PaymentSummaryCards.as_view({ 'get': 'list' })
 payments_recents_table = PaymentRecentsTable.as_view({ 'get': 'list' })
+payments_tracker_chart = PaymentTrackerChart.as_view({ 'get': 'list' })
 
 urlpatterns = [
     path('payments/summary/', payments_summary_cards, name='payments_summary_cards'),
     path('payments/recents/', payments_recents_table, name='payments_recents_table'),
+    path('payments/tracker/', payments_tracker_chart, name='payments_tracker_chart'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from ast import Return
+from datetime import date
 import json
 
 from rest_framework import status, viewsets
@@ -28,7 +30,8 @@ class PaymentSummaryCards(viewsets.ViewSet):
             result[status] = { 
                 'total': query_df['value'].sum(), 
                 'count': query_df['value'].size }
-            result[status]['percentage'] = result[status]['count']/result['Total']['count']*100
+            percentage_part = result[status]['count']/result['Total']['count']
+            result[status]['percentage'] = percentage_part*100
         return Response(result)
 
 
@@ -39,5 +42,51 @@ class PaymentRecentsTable(viewsets.ViewSet):
         merchands_df = pd.DataFrame(merchand_service.get_merchands_mock())
         payments_df = pd.DataFrame(payment_service.get_payments_mock())
         result = pd.merge(merchands_df, payments_df, on='merchand_id', how='inner')
-        result = result.sort_values(by='datetime', ascending=False).tail(10)
-        return Response(json.loads(result.to_json(orient="records")))
+        result = result.sort_values(by='datetime', ascending=True).tail(10).iloc[::-1]
+        result = json.loads(result.to_json(orient="records"))
+        return Response(result)
+
+
+class PaymentTrackerChart(viewsets.ViewSet):
+    permission_classes = (IsAuthenticated,)
+    
+    def list(self, request):
+        period = request.GET.get('period', 'month')
+        payments_df = pd.DataFrame(payment_service.get_payments_mock())
+        payments_df = payments_df.sort_values(by='datetime', ascending=True)
+        
+        if period == 'day':
+            offset = pd.DateOffset(hours=11)
+            str_format = '%Y-%m-%d %H'
+            output_format = '%Hh'
+            range_freq = 'H'
+        elif period == 'week':
+            offset = pd.DateOffset(days=6)
+            str_format = '%Y-%m-%d'
+            output_format = '%a'
+            range_freq = 'D'
+        elif period == 'month':
+            offset = pd.DateOffset(months=11)
+            str_format = '%Y-%m'
+            output_format = '%b'
+            range_freq = 'M'
+
+        # Filtering by period
+        now = pd.Timestamp.now(tz='America/Fortaleza').tz_localize(None)
+        payments_df['datetime'] = pd.to_datetime(payments_df['datetime']).dt.strftime(str_format)
+        initial_time = (now - offset).strftime(str_format)
+        payments_df = payments_df[payments_df['datetime'] > initial_time]
+        
+        # Extracting data
+        periods = pd.date_range(initial_time, now, freq=range_freq).strftime(str_format)
+        datetime_group = payments_df.groupby(by='datetime')
+        payments = datetime_group.sum('value')['value']
+        stores = datetime_group['merchand_id'].nunique()
+        result = pd.merge(payments, stores, left_index=True, right_index=True)
+        result = result.reindex(periods.array, fill_value=0)
+
+        # Formatting and response
+        result = result.reset_index(level=0).iloc[::-1]
+        result['datetime'] = pd.to_datetime(result['datetime']).dt.strftime(output_format)
+        result = json.loads(result.to_json(orient="records"))
+        return Response(result) 
